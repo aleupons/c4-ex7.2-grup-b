a9aaf8b773c72cfdebe419320ea7c5ad162c3522
aleupons <apahoquei@gmail.com>
2021-07-12 14:35:34 +0200

    Endpoint: personas vacunadas en un centro

--- servidor/rutas/personasVacunadas.js
+++ servidor/rutas/personasVacunadas.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getNombreCiudad } = require("../../db/controladores/ciudades");
+const CentroVacunacion = require("../../db/modelos/CentroVacunacion");
 const Ciudad = require("../../db/modelos/Ciudad");
 const Persona = require("../../db/modelos/Persona");
 
@@ -21,6 +22,24 @@ router.get("/ciudad/:idCiudad", async (req, res, next) => {
   } else if (personas.length === 0) {
     const nuevoError = new Error(
       "No tenemos personas vacunadas en esta ciudad"
+    );
+    nuevoError.codigo = 404;
+    return next(nuevoError);
+  }
+  res.json(personas);
+});
+
+router.get("/centro/:idCentro", async (req, res, next) => {
+  const { idCentro } = req.params;
+  const { _id } = await CentroVacunacion.findOne({
+    _id: idCentro,
+  });
+  const personas = await Persona.find({
+    centroVacunacion: _id,
+  });
+  if (personas.length === 0) {
+    const nuevoError = new Error(
+      "No tenemos personas vacunadas en este centro"
     );
     nuevoError.codigo = 404;
     return next(nuevoError);
